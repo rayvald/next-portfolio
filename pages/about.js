85403a9b753a99f9364e2599cc834fdfa3be5063
Osvaldo Murillo <osv.murillo7@gmail.com>
2023-04-20 09:08:50 -0500

    Improvements

--- pages/about.js
+++ pages/about.js
@@ -1,5 +1,11 @@
-import userData from "../constants/data";
+import React from "react";
+import ContainerBlock from "../components/ContainerBlock";
+import AboutMe from "../components/AboutMe";
 
-export default function About() {
-
+export default function about() {
+  return (
+    <ContainerBlock>
+      <AboutMe />
+    </ContainerBlock>
+  );
 }
